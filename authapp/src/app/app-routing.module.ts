40a728d83e68bc239e90612ee15d683403c2937d
javier Fernandez <jfernandez.developer@gmail.com>
2020-01-24 19:15:40 +0100

    Added a guard to protect the navigation

--- authapp/src/app/app-routing.module.ts
+++ authapp/src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PricesComponent } from './components/prices/prices.component';
 import { ProtectedComponent } from './components/protected/protected.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'prices', component: PricesComponent },
-  { path: 'protected', component: ProtectedComponent },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ AuthGuard ] //We put here the guards to apply before navigate to this component
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
